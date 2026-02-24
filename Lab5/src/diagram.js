f38021eeb8646848325761b9dcbe0842727303f8
Emma Ågren <agren.gastronomi@hotmail.com>
2026-02-24 11:58:34 +0100

    Småfix med länkar och namn.

---
+++ Lab5/src/diagram.js
@@ -0,0 +1,18 @@
+
+//Hämta canvas-element.
+const coursesCtx = document.getElementById('coursesChart');
+const programsCtx = document.getElementById('programsChart');
+
+//Stapeldiagram
+new Chart(coursesCtx, {
+    type: 'bar' ,
+    data: {
+        labels: ['Kurs A', 'Kurs B', 'Kurs C'],
+        datasets: [{
+            label: 'Antal sökande',
+            data: [120, 95, 80, 70, 65, 50],
+            borderWidth: 1,
+            backgroundColor: 'rgba(54, 162, 235, 0.6)'
+        }]
+    },
+});
