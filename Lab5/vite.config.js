5bdca7163ea36f0c58da2bcde712af8fe24566ce
Emma Ågren <agren.gastronomi@hotmail.com>
2026-03-02 22:52:55 +0100

    Lägger till de nya html-filerna.

--- Lab5/vite.config.js
+++ Lab5/vite.config.js
@@ -7,7 +7,9 @@ export default defineConfig({
             input: {
                 main: resolve(__dirname, 'index.html'),
                 sass: resolve(__dirname, 'sass.html'),
-                animering: resolve(__dirname, 'animering.html')
+                animering: resolve(__dirname, 'animering.html'),
+                diagram: resolve(__dirname, 'diagram.html'),
+                karta: resolve(__dirname, 'karta.html')
             }
         }
     }
