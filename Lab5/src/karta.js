1466b1e1dbd75b2df2648411857e41f2614dbd2e
Emma Ågren <agren.gastronomi@hotmail.com>
2026-03-02 18:15:15 +0100

    Lägger till funktioner för kordinater och sökning.

--- Lab5/src/karta.js
+++ Lab5/src/karta.js
@@ -3,6 +3,7 @@ const form = document.getElementById("searchForm");
 const input = document.getElementById("locationInput");
 const mapFrame = document.getElementById("mapFrame");
 
+
 /**
  * Hämtar kordinater från Nominatim API baserat på platsnamn.
  * @param {string} Location - Plats som användaren skrivit in.
@@ -11,8 +12,8 @@ const mapFrame = document.getElementById("mapFrame");
 async function fetchCoordinates(location) {
 
     try {
-        const response = await fetch (
-             `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(location)}`
+        const response = await fetch(
+            `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(location)}`
         );
 
         if (!response.ok) {
@@ -35,3 +36,45 @@ async function fetchCoordinates(location) {
         return null;
     }
 }
+
+
+/**
+ * Uppdaterar iframe med OpenStreetMap baserat på koordinater.
+ * @param {string} lat - Latitud
+ * @param {string} lon - Longitud
+ */
+function updateMap(lat, lon) {
+
+    const latitude = parseFloat(lat);
+    const longitude = parseFloat(lon);
+
+    const minLon = longitude - 0.01;
+    const minLat = latitude - 0.01;
+    const maxLon = longitude - 0.01;
+    const maxLat = latitude - 0.01;
+
+    mapFrame.src =
+        `https://www.openstreetmap.org/export/embed.html?marker=${latitude}%2C${longitude}&layer=mapnik`;
+}
+
+
+/**
+ * Hanterar formulär-sökning
+ * @param {Event} event
+ */
+async function handleSearch(event) {
+    event.preventDefault();
+
+    const location = input.value.trim();
+    if (!location) return;
+
+    const coordinates = await fetchCoordinates(location);
+
+    if (coordinates) {
+        updateMap(coordinates.lat, coordinates.lon);
+    }
+}
+
+
+//Event listener
+form.addEventListener("submit", handleSearch);
