f538b2e9e59d83162b04baf3cbec396affcdfa31
Emma Ågren <agren.gastronomi@hotmail.com>
2026-03-02 15:18:02 +0100

    Färdigställt funktion för att hämta kordinater.

--- Lab5/src/karta.js
+++ Lab5/src/karta.js
@@ -8,6 +8,30 @@ const mapFrame = document.getElementById("mapFrame");
  * @param {string} Location - Plats som användaren skrivit in.
  * @return {Promise<{Lat: string, Lon: string}>}
  */
-async function name(params) {
-    
+async function fetchCoordinates(location) {
+
+    try {
+        const response = await fetch (
+             `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(location)}`
+        );
+
+        if (!response.ok) {
+            throw new Error("Fel vid hämtning av koordinater");
+        }
+
+        const data = await response.json();
+
+        if (data.length === 0) {
+            alert("Ingen plats hittades.");
+            return null;
+        }
+
+        return {
+            lat: data[0].lat,
+            lon: data[0].lon
+        };
+    } catch (error) {
+        console.error("Ett fel uppstod:", error);
+        return null;
+    }
 }
